c95c3aa7bb56a60cccb21bbdb057af27a161021a
Kevin Souza <kevinwillyansouza@gmail.com>
2024-06-22 14:39:02 -0400

    feat: criação dos componentes de card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,25 @@
-function Card() {
-    return <></>
+import './Card.css'
+
+interface CardProps {
+    title: string
+    body: string
+    url: string
+}
+
+function Card({ title, body, url }: CardProps) {
+    return (
+        <article className="card">
+            <div className="card-title">{title}</div>
+            <div className="card-body">
+                <p>{body}</p>
+            </div>
+            <div className="card-url">
+                <a href={url} target="_blank" rel="noreferrer">
+                    Abrir link
+                </a>
+            </div>
+        </article>
+    )
 }
 
 export default Card
